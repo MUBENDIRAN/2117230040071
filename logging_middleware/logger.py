ef296ce8ee60ca29de07bdf09e74c0eb0fded5f7
MUBENDIRAN <mubemubendiran@gmail.com>
2026-05-06 11:46:50 +0530

    the vehicle maintenance scheduler service is build with retry mechanism async processing using fastapi framework

--- logging_middleware/logger.py
+++ logging_middleware/logger.py
@@ -1,9 +1,13 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJtdWJlbmRpcmFuLmsuMjAyMy5lY2VAcml0Y2hlbm5haS5lZHUuaW4iLCJleHAiOjE3NzgwNDQ0NzEsImlhdCI6MTc3ODA0MzU3MSwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjFlYzI1ZDIyLTcwNGEtNDAxZC1iZDA5LTEwMDQzNTRmNmQzZSIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6Im11YmVuZGlyYW4gayIsInN1YiI6IjYxZTA0OTJhLTE4YzktNGQ5MS04YWU5LWJhZjkzNTQ1YWYyNSJ9LCJlbWFpbCI6Im11YmVuZGlyYW4uay4yMDIzLmVjZUByaXRjaGVubmFpLmVkdS5pbiIsIm5hbWUiOiJtdWJlbmRpcmFuIGsiLCJyb2xsTm8iOiIyMTE3MjMwMDQwMDcxIiwiYWNjZXNzQ29kZSI6IkJUQ0RxVCIsImNsaWVudElEIjoiNjFlMDQ5MmEtMThjOS00ZDkxLThhZTktYmFmOTM1NDVhZjI1IiwiY2xpZW50U2VjcmV0IjoiSEdkU1djRnBralZ4cUVORyJ9.OpPmKYGAbdRHyCjxcr6f2NurNtZYrmnygJuQwUc0t_w"
+load_dotenv()
+
+TOKEN=os.getenv("ACCESS_TOKEN")
 
 def log(stack,level,package,message):
-    url = "http://20.297.122.291/evaluation-service/logs"
+    url = "https://20.244.56.144/evaluation-service/logs"
 
     payload={
         "stack": stack,
@@ -12,18 +16,27 @@ def log(stack,level,package,message):
         "message": message
     }
 
-    headers = {
+    HEADERS = {
         "Authorization": f"Bearer {TOKEN}",
         "Content-Type": "application/json"
     }
 
     try:
         response=requests.post(
-            URL,
+            url,
             json=payload,
             headers=headers
         )
 
         print(response.json())
 
-    
+    except Exception as e:
+        print("Logging failed:",e)
+
+
+log(
+    "backend",
+    "info",
+    "middleware",
+    "Logger test successful"
+)
---
+++ vehicle_maintenance_schedular/app.py
@@ -0,0 +1,98 @@
+from fastapi import FastAPI
+import os
+import requests
+from dotenv import load_dotenv
+import sys
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__),"..")
+    )
+)
+from logging_middleware.logger import log
+
+
+load_dotenv()
+
+TOKEN=os.getenv("ACCESS_TOKEN")
+
+header={
+    "Authorization":f"Bearer{TOKEN}"
+}
+
+DEPOT_URL="http://20.244.56.144/evaluation-service/depots"
+VEHICLE_URL="http://20.244.56.144/evaluation-service/vehicles"
+
+app = FastAPI()
+
+@app.get("/schedule")
+def schedule():
+    try:
+        log(
+            "backend",
+            "info",
+            "service",
+            "Fetching depots"
+        )
+
+        depot_response=requests.get(
+            DEPOT_URL,
+            headers=HEADERS
+        )
+
+        vehicle_response=requests.get(
+            VEHICLE_URL,
+            headers=HEADERS
+        )
+
+        depots=depots_response.json()["deposts"]
+        vehicles=vehicle_response.json()["vehicles"]
+        mechanic_hours=depots[0]["MechanicHours"]
+
+        log(
+            "backend",
+            "info",
+            "service",
+            f"Mechanic hours: {mechanic_hours}"
+        )
+
+        n=len(vehicles)
+        
+        dp=[[0]*(mechanic_hours+1) for _ in range(n+1)]
+        
+        for i in range(1,n+1):
+            duration=vehicles[i-1]["Duration"]
+            impact=vehicles[i-1]["Impact"]
+
+            for j in range(mechanic_hours+1):
+                if duration<=j:
+                    dp[i][j]=max(
+                        impact+dp[i-1][j-duration],
+                        dp[i-1][j]
+                    )
+
+                else:
+                    dp[i][j]=dp[i-1][j]
+
+        max_impact=dp[n][mechanic_hours]
+
+        log(
+            "backend",
+            "info",
+            "service",
+            "Scheduling completed successfully"
+        )
+
+        return{
+        "mechanicHours": mechanic_hours,
+        "maxImpact": max_impact   
+        }
+
+    except Exception as e:
+        log(
+            "backend",
+            "error",
+            "handler",
+            str(e)
+        )
+
+        return {"error":str(e)} 
